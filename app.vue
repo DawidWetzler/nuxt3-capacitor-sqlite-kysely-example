7939663bb66ba60d8a1863c3f2dad1c878b98e8d
DawidWetzler <wetzlerdawid@gmail.com>
2023-11-05 13:29:20 +0100

    feat: transaction examples

--- app.vue
+++ app.vue
@@ -1,4 +1,4 @@
-🪈<script lang="ts" setup>
+<script lang="ts" setup>
 import { sql } from 'kysely';
 import Database, { sqlite } from '~/database';
 import Migrator from '~/database/BrowserMigrator';
@@ -13,15 +13,53 @@ onMounted(async () => {
   console.log(await Database.selectFrom('test').selectAll().execute());
 
   // raw queries
-  console.log(await sql`INSERT INTO test VALUES (null, 'Dawid')`.execute(Database));
+  console.log(await sql`INSERT INTO test VALUES (null, 'Dawid') RETURNING *`.execute(Database));
 
+  // you can also use transactions
+  await Database.transaction().execute(async (trx) => {
+    await trx.insertInto('test').values({ name: 'Sami' }).returning('id')
+      .executeTakeFirstOrThrow();
+
+    return await trx.insertInto('test').values({ name: 'commit' }).returningAll().execute();
+  });
+
+  console.log(await Database.selectFrom('test').selectAll().execute());
+
+  try {
+
+    // and rollback
+    await Database.transaction().execute(async (trx) => {
+      await trx.insertInto('test').values({ name: 'Rollback' }).execute();
+      await trx.insertInto('test').values({ name: 'Rollback' }).execute();
+      throw 1;
+    })
+
+  } catch (e) {
+    //
+  }
+
+
+  console.log(await Database.selectFrom('test').selectAll().execute());
 
   // this also works but...
   // if the migrator is user you have to ensure that migrator runs before all the queries
   // will be executed therefore it's probably better to use sql from kysely
-
   const db = await sqlite.retrieveConnection('nuxt', false);
-  await db.run("INSERT INTO `test` VALUES (null, 'Jean')");
+
+  await db.beginTransaction();
+  await db.query("INSERT INTO `test` VALUES (null, ?)", ['Sami']);
+  await db.query("INSERT INTO `test` VALUES (null, ?)", ['commit']);
+  await db.commitTransaction();
+
+  console.log(await db.query('SELECT * FROM test'));
+
+  await db.beginTransaction();
+  await db.query("INSERT INTO `test` VALUES (null, ?)", ['Rollback']);
+  await db.query("INSERT INTO `test` VALUES (null, ?)", ['Rollback']);;
+  await db.rollbackTransaction();
+
+  await db.execute("INSERT INTO `test` VALUES (null, 'Jean')");
+
   console.log(await db.query('SELECT `id`, `name` FROM test'));
 });
 
